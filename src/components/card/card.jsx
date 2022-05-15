1b1ae1adf34cedb15c4801dc216a7930fa706178
myplacecoffee <74812757+myplacecoffee@users.noreply.github.com>
2022-05-16 00:17:01 +0900

    commit

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import styles from './card.module.css';
 
+const DEFAULT_IMAGE='/images/default_logo.png';
+
 const Card = ({}) => {
     //카드프리뷰 최종애들
+    const url=DEFAULT_IMAGE;
     return(
-        <div>
-            <p>card.title</p>
-            <p>card.content</p>
+        <div className={styles.card}>
+        {/* <div className={`${styles.card} ${getStyles(theme)}`}> */}
+            {/* <p className={styles.title}>Card Preview</p> */}
+            <img className={styles.avatar} src={url} alt="profile photo"/>
+            <div className={styles.info}>
+                <h1 className={styles.name}>name</h1>
+                <p className={styles.company}>company</p>
+                <p className={styles.job}>job</p>
+                <p className={styles.email}>email</p>
+                <p className={styles.greeting}>greeting</p>
+            </div>
         </div>
 
     );
 };
+
+// function getStyles(theme){
+//     switch(theme){
+//         case 'dark':
+//             return styles.dark;
+//         case 'light':
+//             return styles.light;
+//         case 'colorful':
+//             return styles.colorful;
+//         default:
+//             throw new Error(`unknown theme: ${theme}`);
+//     }
+// }
 
 export default Card;
